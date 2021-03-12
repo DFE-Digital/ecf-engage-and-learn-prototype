e508fede0aedeec5bb828b6a388725fce06d2955
Dominique <dominique.sherwood@gmail.com>
2021-03-12 16:16:36 +0000

    Branching, home banner, Week 2 >3, part of part 3

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const router = express.Router()
 
+
+router.post('/finish-session-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var lessonPart4 = req.session.data['finish-session']
+
+  // Check whether the variable matches a condition
+  if (lessonPart4 == "yes") {
+    // Send user to next page
+    res.redirect('/it3/lesson-list-complete')
+  }
+
+  if (lessonPart4 == "discussion") {
+    // Send user to next page
+    res.redirect('/it3/lesson-list-discussion')
+  }
+  else {
+    // Send user to ineligible page
+    res.redirect('/it3/lesson-list')
+  }
+
+})
 // Add your routes here - above the module.exports line
 
 module.exports = router

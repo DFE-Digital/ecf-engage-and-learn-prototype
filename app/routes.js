b6b1cdda7a9452fcfc920c497b312103f95ad9d7
Dominique <dominique.sherwood@gmail.com>
2021-03-30 17:26:36 +0100

    routes

--- app/routes.js
+++ app/routes.js
@@ -23,6 +23,28 @@ router.post('/finish-session-answer', function (req, res) {
   }
 
 })
+
+router.post('/finish-session-answer-it4', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var lessonPart4 = req.session.data['finish-session']
+
+  // Check whether the variable matches a condition
+  if (lessonPart4 == "yes") {
+    // Send user to next page
+    res.redirect('/it4/lesson-list-complete')
+  }
+
+  if (lessonPart4 == "discussion") {
+    // Send user to next page
+    res.redirect('/it4/lesson-list-todo')
+  }
+  else {
+    // Send user to ineligible page
+    res.redirect('/it4/lesson-list-inprogress')
+  }
+
+})
 // Add your routes here - above the module.exports line
 
 module.exports = router

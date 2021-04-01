7eb0c3b53fe0c3500e1789958fbf46107b7cc942
Dominique <dominique.sherwood@gmail.com>
2021-04-01 11:55:38 +0100

    fix guidance and added "coaching title"

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Early Career Framework',
+  serviceName: 'Coaching and mentoring for early career teachers',
 
   // Default port that prototype runs on
   port: '3000',
